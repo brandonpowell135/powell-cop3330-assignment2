999e3f925317b04015636e63000b3759de02c764
Brandon Desktop <brandonpowel35@gmail.com>
2021-06-11 09:15:58 -0400

    multi unit tests

---
+++ src/main/java/assignment2/ex26/base/solution26.java
@@ -0,0 +1,4 @@
+package assignment2.ex26.base;
+
+public class solution26 {
+}
--- src/test/java/assignment2/ex25/base/PasswordCheckerTest.java
+++ src/test/java/assignment2/ex25/base/PasswordCheckerTest.java
@@ -16,10 +16,28 @@ class PasswordCheckerTest {
         assertEquals(expected,actual);
     }
     @Test
+    void Very_Weak_Password_2() {
+        PasswordChecker pass = new PasswordChecker();
+
+        int actual = pass.passwordValidator("54799");
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+    @Test
     void Weak_Password() {
         PasswordChecker pass = new PasswordChecker();
 
         int actual = pass.passwordValidator("abcdef");
+        int expected = 2;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void Weak_Password_2() {
+        PasswordChecker pass = new PasswordChecker();
+
+        int actual = pass.passwordValidator("hsgtss");
         int expected = 2;
 
         assertEquals(expected, actual);
@@ -34,6 +52,15 @@ class PasswordCheckerTest {
         assertEquals(expected, actual);
     }
     @Test
+    void Strong_Password_2() {
+        PasswordChecker pass = new PasswordChecker();
+
+        int actual = pass.passwordValidator("wow867wow");
+        int expected = 3;
+
+        assertEquals(expected, actual);
+    }
+    @Test
     void Very_Strong_Password() {
         PasswordChecker pass = new PasswordChecker();
 
@@ -43,10 +70,28 @@ class PasswordCheckerTest {
         assertEquals(expected, actual);
     }
     @Test
+    void Very_Strong_Password_2() {
+        PasswordChecker pass = new PasswordChecker();
+
+        int actual = pass.passwordValidator("88hh^^%%v");
+        int expected = 4;
+
+        assertEquals(expected, actual);
+    }
+    @Test
     void Unusaul_Password() {
         PasswordChecker pass = new PasswordChecker();
 
         int actual = pass.passwordValidator("abcdefghijk");
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void Unusaul_Password_2() {
+        PasswordChecker pass = new PasswordChecker();
+
+        int actual = pass.passwordValidator("123456789");
         int expected = 0;
 
         assertEquals(expected, actual);
